acf96f30368936050653fe46723f216c8e7c5144
jmt75200 <joelle@pcgamerzhawaii.com>
2015-01-13 18:56:47 -1000

    maximum function passes test

--- functions.js
+++ functions.js
@@ -174,6 +174,15 @@ function minimum(x,y){
  * @param {number} y
  * @return {number} the largest number
  */
+function maximum(x,y){
+  if (x >= y){
+    return x;
+  }else{
+    return y;
+  }
+}
+
+
 
 /**
  * Returns true if `n` is even.
